c421109faf6f77d6008a9bb08429141c941f0bdd
Kirill Zorin <thesecretsoftheuniverse@mail.ru>
2023-05-20 13:29:36 +0300

    add storage intf, storage paths struct

---
+++ storage/files/files.go
@@ -0,0 +1,110 @@
+package files
+
+import (
+	"article-storage-bot/lib/e"
+	"article-storage-bot/storage"
+	"encoding/gob"
+	"errors"
+	"fmt"
+	"math/rand"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+const perm = 0755
+
+var ErrNoSavedPages = errors.New("no one saved page")
+
+type Storage struct {
+	basePath string
+}
+
+func (s Storage) Save(p *storage.Page) (err error) {
+	fp := filepath.Join(s.basePath, p.UserName)
+	if err := os.MkdirAll(fp, perm); err != nil {
+		return e.Wrap("can't save page", err)
+	}
+	fName, err := filename(p)
+	if err != nil {
+		return err
+	}
+	fp = filepath.Join(fp, fName)
+	file, err := os.Create(fp)
+	if err != nil {
+		return err
+	}
+	defer func() { _ = file.Close() }() // ignore
+
+	if err := gob.NewEncoder(file).Encode(p); err != nil {
+		return e.Wrap("can't encode page", err)
+	}
+	return nil
+}
+
+func (s Storage) PickRandom(uName string) (*storage.Page, error) {
+	fp := filepath.Join(s.basePath, uName)
+	files, err := os.ReadDir(fp)
+	if err != nil {
+		return nil, err
+	}
+	if len(files) == 0 {
+		return nil, ErrNoSavedPages
+	}
+
+	rand.NewSource(time.Now().UnixNano())
+	n := rand.Intn(len(files))
+	file := files[n]
+	return s.decodePage(filepath.Join(fp, file.Name()))
+}
+
+func (s Storage) Remove(p *storage.Page) error {
+	const msg = "can't remove page"
+	fName, err := filename(p)
+	if err != nil {
+		return e.Wrap(msg, err)
+	}
+	fp := filepath.Join(s.basePath, fName)
+	if err := os.Remove(fp); err != nil {
+		return e.Wrap(fmt.Sprintf("%s %s", msg, fp), err)
+	}
+	return nil
+}
+
+func (s Storage) IsExists(p *storage.Page) (bool, error) {
+	const msg = "can't check exists page"
+	fName, err := filename(p)
+	if err != nil {
+		return false, e.Wrap(msg, err)
+	}
+
+	fp := filepath.Join(s.basePath, fName)
+	switch _, err := os.Stat(fp); {
+	case errors.Is(err, os.ErrNotExist):
+		return false, nil
+	case err != nil:
+		return true, e.Wrap(fmt.Sprintf("%s %s", msg, fp), err)
+	}
+	return true, nil
+}
+
+func NewStorage(bp string) Storage {
+	return Storage{basePath: bp}
+}
+
+func filename(p *storage.Page) (string, error) {
+	return p.Hash()
+}
+
+func (s Storage) decodePage(fp string) (*storage.Page, error) {
+	f, err := os.Open(fp)
+	if err != nil {
+		return nil, e.Wrap(fmt.Sprintf("can't decode page %s", fp), err)
+	}
+	defer func() { _ = f.Close() }()
+	p := new(storage.Page)
+	if err := gob.NewDecoder(f).Decode(p); err != nil {
+		return nil, e.Wrap("can't decode page", err)
+	}
+	return p, nil
+}
---
+++ storage/storage.go
@@ -0,0 +1,32 @@
+package storage
+
+import (
+	"article-storage-bot/lib/e"
+	"crypto/md5"
+	"fmt"
+	"io"
+)
+
+type Storage interface {
+	Save(p *Page) error
+	PickRandom(uName string) (*Page, error)
+	Remove(p *Page) error
+	IsExists(p *Page) (bool, error)
+}
+
+type Page struct {
+	URL      string
+	UserName string
+}
+
+func (p Page) Hash() (string, error) {
+	const msg = "Can't calculate hash"
+	h := md5.New()
+	if _, err := io.WriteString(h, p.UserName); err != nil {
+		return "", e.Wrap(msg, err)
+	}
+	if _, err := io.WriteString(h, p.URL); err != nil {
+		return "", e.Wrap(msg, err)
+	}
+	return fmt.Sprintf("%x", h.Sum(nil)), nil
+}
